5c278a1695aef59eccf1a5c36b6b280b4fc45c4d
AxelVallesBiomedic <axma_cv@hotmail.com>
2025-09-15 13:11:52 -0600

    Added second program to solution, updated first problem solution to better use string tools to show results in a simpler way

--- day 1/DesafioString_1.29/DesafioString_1.29/Program1.cs
+++ day 1/DesafioString_1.29/DesafioString_1.29/Program1.cs
@@ -12,5 +12,4 @@ Console.WriteLine($"{var1}, {var2}");
 
 var3 = var1.IndexOf(var2);
 
-Console.Write("El indice de la primera aparicion del caracter buscado en el string es: ");
-Console.WriteLine(var3);
+Console.Write($"El indice de la primera aparicion del caracter buscado en el string es: {var3}");
---
+++ day 1/DesafioString_1.29/DesafioString_1.29/Program2.cs
@@ -0,0 +1,11 @@
+﻿
+
+Console.WriteLine("Ingrese su nombre");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese su apellido");
+string apellido = Console.ReadLine();
+
+string nombreCompleto = string.Concat(nombre + " " + apellido);
+
+Console.WriteLine($"Su nombre completo es: {nombreCompleto}");
