fa0aed000d256790a7bdd5d8a5478b4a0500450e
AxelVallesBiomedic <axma_cv@hotmail.com>
2025-09-15 12:28:39 -0600

    Added challenge 1.27 solution

---
+++ day 1/DesafioString_1.27/DesafioString_1.27/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string var1;
+
+
+Console.WriteLine("Por favor ingresa tu nombre y presiona ENTER");
+var1 = Console.ReadLine();
+string var2 = " " + var1 + " ";
+
+Console.WriteLine(var1.ToUpper());
+Console.WriteLine(var1.ToLower());
+Console.WriteLine(var2.Trim());
+Console.WriteLine(var1.Substring(3));
+
+
