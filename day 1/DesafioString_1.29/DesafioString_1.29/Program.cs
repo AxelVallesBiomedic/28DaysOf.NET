ad8050016ae29c919bc1ccc38f7af1db5a4a2f0a
AxelVallesBiomedic <axma_cv@hotmail.com>
2025-09-15 12:44:59 -0600

    Added provisional version of first console app solution

---
+++ day 1/DesafioString_1.29/DesafioString_1.29/Program.cs
@@ -0,0 +1,16 @@
+﻿string var1;
+string var2;
+int var3;
+
+Console.Write("Ingrese un texto aqui: ");
+var1 = Console.ReadLine();
+
+Console.Write("Ingrese la letra buscada:");
+var2 = Console.ReadLine();
+
+Console.WriteLine($"{var1}, {var2}");
+
+var3 = var1.IndexOf(var2);
+
+Console.Write("El indice de la primera aparicion del caracter buscado en el string es: ");
+Console.WriteLine(var3);
