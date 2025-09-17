4b7cd2f2ad02e03e80451b421b3f723a79a57f40
AxelVallesBiomedic <axma_cv@hotmail.com>
2025-09-17 14:38:43 -0600

    Added Solution for task 43 in chapter 3

---
+++ day 3/DesafioC#_3.43/DesafioC#_3.43/Program.cs
@@ -0,0 +1,12 @@
+﻿int num1;
+int num2;
+int suma;
+
+Console.Write("Programa para sumar dos numeros \nIngrese primer numero: ");
+num1 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese segundo numero:");
+num2 = int.Parse(Console.ReadLine());
+
+suma = num1 + num2;
+
+Console.WriteLine($"La suma de tus numeros es: {suma}");
