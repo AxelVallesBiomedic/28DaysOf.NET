9336f03188a32c1b0fac771b7eda961eecf5b393
AxelVallesBiomedic <axma_cv@hotmail.com>
2025-09-17 13:38:00 -0600

    Added solution for task 42 on chapter 3

---
+++ day 1/DesafioC#_3.41/DesafioC#_3.41/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string amigo1 = "Juan";
+        string amigo2 = "Jorge";
+        string amigo3 = "Jose";
+
+        SaludarAmigo(amigo1);
+        SaludarAmigo(amigo2);
+        SaludarAmigo(amigo3);
+    }
+
+    public static void SaludarAmigo(string nombre)
+    {
+        Console.WriteLine($"¡Hola {nombre}, que tengas un buen dia!");
+    }
+}
