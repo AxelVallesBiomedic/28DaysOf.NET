1808cfe3389f344527fa09699e5b37ad678a0a99
AxelVallesBiomedic <axma_cv@hotmail.com>
2025-09-19 16:19:24 -0600

    Created solution for task, except that newest version of .net does not throw a dividedbyzero exception and instead prints an infinite symbol.

---
+++ day 3/DesafioC#_3.44/DesafioC#_3.44/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DesafioC__3._44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, resultado;
+
+            int resultadoInt;
+            Console.WriteLine("Ingresa el numerando: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresa el denominador: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            resultado = Division(a, b);
+            Console.WriteLine($"El resultado de la division es: {resultado}");
+
+            resultadoInt = (int)resultado;
+
+        }
+
+        public static double Division(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
