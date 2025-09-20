7a6ed4506fff8986b6ea3ecb19f4f7b31f0b7aca
AxelVallesBiomedic <axma_cv@hotmail.com>
2025-09-19 21:02:27 -0600

    Created simple program that asks for your score, and compares to a hardcoded previous high score, if yours is bigger it overwrittes the name and score of the biggest winner

---
+++ day 4/DesafioC#_4.56_PuntuacionMasAlta/DesafioC#_4.56_PuntuacionMasAlta/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace DesafioC__4._56_PuntuacionMasAlta
+{
+    internal class Program
+    {
+        static string nombre;
+        static int puntaje;
+
+        static int puntajeMasAlto = 69420;
+        static string nombreGanadorMaximo = "Joe Mama";
+
+        static void Main(string[] args)
+        {
+            PuntajeObtenido();
+        }
+
+        static void PuntajeObtenido()
+        {
+            Console.WriteLine("Ingrese puntaje obtenido");
+            puntaje = Convert.ToInt32(Console.ReadLine());
+
+            if(puntaje > puntajeMasAlto)
+            {
+                Console.WriteLine("Nuevo puntaje maximo obtenido!!");
+                while (string.IsNullOrEmpty(nombre))
+                {
+                    Console.WriteLine("Ingrese Su nombre: ");
+                    nombre = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(nombre))
+                    {
+                        Console.WriteLine("El nombre no puede estar vacio");
+                    }                
+                }
+                puntajeMasAlto = puntaje;
+                nombreGanadorMaximo = nombre;
+
+                Console.WriteLine($"La puntuacion mas alta ahora es de {puntajeMasAlto}, obtenida por el jugador {nombreGanadorMaximo}");
+            }
+
+            else
+            {
+                Console.WriteLine($"La puntuacion mas alta sigue siendo {puntajeMasAlto}, obtenida por el jugador {nombreGanadorMaximo}");
+            }
+        }
+    }
+}
