9aec8e4b1648992b4e4af522b83741c97d5912e3
AxelVallesBiomedic <axma_cv@hotmail.com>
2025-09-19 20:06:45 -0600

    Created primitive login system that does not make use of stored data, instead first makes a register storing values on variables, then makes a login comparing variables with readline values

---
+++ day 3/DesafioC#_4.53_LoginUsuario/DesafioC#_4.53_LoginUsuario/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace DesafioC__4._53_LoginUsuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? usuarioRegistro;
+            string? passwordRegistro;
+            bool logexitoso = false;
+            string? user;
+            string password;
+
+
+            Console.WriteLine("=== Registro de Usuario ===");
+            Console.Write("Ingrese un nombre de usuario: ");
+            usuarioRegistro = Console.ReadLine();
+            Console.Write("Ingrese una contraseña: ");
+            passwordRegistro = Console.ReadLine();
+
+            while (logexitoso == false)
+            { 
+            
+                Console.WriteLine("\n=== Inicio de Sesión ===");
+                Console.Write("Ingrese su nombre de usuario: ");
+
+                user = Console.ReadLine();
+
+                Console.Write("Ingrese su contraseña: ");
+                password = Console.ReadLine();
+
+                if (user == usuarioRegistro && password == passwordRegistro)
+                {
+                    Console.WriteLine($"Inicio de sesión exitoso, ¡Bienvenido {user}!");
+                    logexitoso = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Error: Nombre de usuario o contraseña incorrectos. Intente de nuevo.");
+                }
+
+            }
+
+        }
+    }
+}
