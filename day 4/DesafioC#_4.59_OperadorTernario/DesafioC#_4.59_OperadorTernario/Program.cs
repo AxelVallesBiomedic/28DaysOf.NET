eb800d7b89fff8e0f976596713f557b8ab7b9e3b
AxelVallesBiomedic <axma_cv@hotmail.com>
2025-09-19 21:37:28 -0600

    Created simmple program to report how the person feels the temperature based on temperature value typed by user, if the value is not numerical, the program exits.

---
+++ day 4/DesafioC#_4.59_OperadorTernario/DesafioC#_4.59_OperadorTernario/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DesafioC__4._59_OperadorTernario
+{
+    internal class Program
+    {
+        static int temperatura;
+        static string sensacionTermica;
+
+        static void Main(string[] args)
+        {
+           temperatura = IsNumber();
+            SensacionTermica(temperatura);
+        }
+
+        static int IsNumber()
+        {
+            Console.WriteLine("Que temperatura hay afuera? Escribe el valor en numeros");
+
+            string? typedValue = Console.ReadLine();
+
+            bool isNum = int.TryParse(typedValue, out temperatura);
+
+            if (isNum == false)
+            {
+                Console.WriteLine("Ese valor no es un numero, error");
+                Environment.Exit(1);
+            }
+            return temperatura;
+        }
+
+        static void SensacionTermica(int temperatura)
+        {
+            sensacionTermica = temperatura <= 15 ? "Hace mucho frio" : temperatura >= 16 && temperatura <= 28 ? "Hace un clima agradable" : "Hace mucho calor";
+
+            Console.WriteLine(sensacionTermica);
+        }
+    }
+}
